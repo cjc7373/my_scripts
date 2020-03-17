67ed634cb0de6deebde46e235f4af37d11529f29
cjc7373 <niuchangcun@163.com>
2020-03-17 13:30:10 +0800

    optimize when updating caddy

--- caddy.sh
+++ caddy.sh
@@ -11,61 +11,61 @@ if [ `whoami` != "root" ];then
 fi
 
 wget --no-check-certificate -c -t3 -T60 -O "caddy" \
-"https://github.com/caddyserver/caddy/releases/download/v2.0.0-beta.15/caddy2_beta15_linux_amd64"
+"https://github.com/caddyserver/caddy/releases/download/v2.0.0-beta.17/caddy2_beta17_linux_amd64"
 if [ $? -eq 0 ]; then
     echo -e "[${green}Info${plain}] ${filename} download completed..."
 fi
 chmod +x caddy
 mv caddy /usr/bin/
 
-groupadd --system caddy
-useradd --system \
-    --gid caddy \
-    --create-home \
-    --home-dir /var/lib/caddy \
-    --shell /usr/sbin/nologin \
-    --comment "Caddy web server" \
-    caddy
+if [ ! -e /etc/caddy/Caddyfile ]; then
+    groupadd --system caddy
+    useradd --system \
+        --gid caddy \
+        --create-home \
+        --home-dir /var/lib/caddy \
+        --shell /usr/sbin/nologin \
+        --comment "Caddy web server" \
+        caddy
 
-if [ ! -d /etc/caddy ]; then
     mkdir -p /etc/caddy
-fi
-
-cat > /etc/caddy/Caddyfile << EOF
+    cat > /etc/caddy/Caddyfile << EOF
 0.0.0.0:2020 {
     respond "Hello, world!"
 }
 EOF
 
-# Create log folder
-chmod 755 /root
-if [ ! -d /root/caddy ]; then
+    # Create log folder
+    chmod 755 /root
     mkdir -p /root/caddy
+    chown caddy /root/caddy
+
+    cat > /etc/systemd/system/caddy.service <<- EOF
+        [Unit]
+        Description=Caddy Web Server
+        Documentation=https://caddyserver.com/docs/
+        After=network.target
+
+        [Service]
+        User=caddy
+        Group=caddy
+        ExecStart=/usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile --resume --environ
+        ExecReload=/usr/bin/caddy reload --config /etc/caddy/Caddyfile --adapter caddyfile
+        TimeoutStopSec=5s
+        LimitNOFILE=1048576
+        LimitNPROC=512
+        PrivateTmp=true
+        ProtectSystem=full
+        AmbientCapabilities=CAP_NET_BIND_SERVICE
+
+        [Install]
+        WantedBy=multi-user.target
+    EOF
+
+    systemctl daemon-reload
+    systemctl enable caddy
+    systemctl start caddy
+
+else
+    systemctl restart caddy
 fi
-chown caddy /root/caddy
-
-cat > /etc/systemd/system/caddy.service <<- EOF
-    [Unit]
-    Description=Caddy Web Server
-    Documentation=https://caddyserver.com/docs/
-    After=network.target
-
-    [Service]
-    User=caddy
-    Group=caddy
-    ExecStart=/usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile --resume --environ
-    ExecReload=/usr/bin/caddy reload --config /etc/caddy/Caddyfile --adapter caddyfile
-    TimeoutStopSec=5s
-    LimitNOFILE=1048576
-    LimitNPROC=512
-    PrivateTmp=true
-    ProtectSystem=full
-    AmbientCapabilities=CAP_NET_BIND_SERVICE
-
-    [Install]
-    WantedBy=multi-user.target
-EOF
-
-systemctl daemon-reload
-systemctl enable caddy
-systemctl start caddy
