4e58ba4bd366d4d3a25da62110d44a9fb1b690af
cjc7373 <niuchangcun@gmail.com>
2022-01-20 11:22:18 +0800

    beautify coding time statistic

--- active/coding_time_statistic.py
+++ active/coding_time_statistic.py
@@ -2,37 +2,62 @@
 统计 Github 上自己所有 commits 的时间分布.
 """
 import requests
+import shutil
 from datetime import datetime
 
-statistic = [0 for i in range(24)]
-all_cnt = 0
 
-page = 1
-while True:
-    url = f"https://api.github.com/search/commits?q=author:cjc7373&per_page=100&page={page}"
-    print(f"Reading page {page}")
-    r = requests.get(url)
-    items = r.json()["items"]
+def fetch_data(user: str) -> list[str]:
+    page = 1
+    res: list[str] = []
+    while True:
+        url = f"https://api.github.com/search/commits?q=author:{user}&per_page=100&page={page}"
+        print(f"Reading page {page}")
+        r = requests.get(url)
+        items = r.json()["items"]
 
-    page += 1
-    if not items:
-        break
+        page += 1
+        if not items:
+            return res
 
-    for item in items:
-        date_str = item["commit"]["committer"]["date"]
-        # print(date_str)
+        for item in items:
+            date_str = item["commit"]["committer"]["date"]
+            # print(date_str)
+            res.append(date_str)
+
+
+def generate_statistic(data: list[str]) -> tuple[list[int], int]:
+    statistic = [0 for i in range(24)]
+    all_cnt = 0
+    for date_str in data:
         try:
             t = datetime.fromisoformat(date_str)
         except ValueError as e:
             # may encounter something like 2021-02-11T08:55:12.000Z
-            print(e)
+            print(f"WARNING: {e}")
         # print(t.hour)
         statistic[t.hour] += 1
         all_cnt += 1
+    return statistic, all_cnt
 
-statistic = [i * 200 // all_cnt for i in statistic]
-for index, data in enumerate(statistic):
-    print(f"{index:2} - {index+1:2}: ", end="")
-    for i in range(data):
-        print("▇", end="")
+
+def print_result(statistic, all_cnt):
+    terminal_width, _ = shutil.get_terminal_size((80, 24))
+    scale = (terminal_width - 10) / max(statistic)
+    statistic = [int(i * scale) for i in statistic]
+    for index, data in enumerate(statistic):
+        print(f"{index:2} - {index+1:2}: ", end="")
+        for i in range(data):
+            print("▇", end="")
+        print()
+    # 打印横坐标, 间距为 10
+    print("Commits: ", end="")
+    for i in range(0, (terminal_width - 10), 10):
+        print(f"{int(i/scale):<10}", end="")
     print()
+
+
+if __name__ == "__main__":
+    data = fetch_data("cjc7373")
+    statistic, all_cnt = generate_statistic(data)
+    # print(statistic, all_cnt)
+    print_result(statistic, all_cnt)
