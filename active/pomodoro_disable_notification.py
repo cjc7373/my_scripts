45ab74ea715b7c2fd2449b68596215ce9619384f
cjc7373 <niuchangcun@gmail.com>
2022-04-24 22:23:02 +0800

    pomodoro: add notification

--- active/pomodoro_disable_notification.py
+++ active/pomodoro_disable_notification.py
@@ -2,6 +2,7 @@
 import dbus
 import time
 import sys
+import subprocess
 from datetime import datetime, timedelta
 from rich import print  # override built-in print
 from rich.progress import track
@@ -28,17 +29,43 @@ def uninhibit(arg_0: int) -> None:
     notify_interface.UnInhibit(arg_0)
 
 
+def notify(message: str) -> None:
+    # FIXME: hints like `"sound-file": "/usr/share/sounds/Oxygen-Sys-App-Error.ogg"`
+    # is not working. Don't know why.
+    # So I have to use mpv..
+    # Anyway, sound is not so much important.
+    notify_interface.Notify(
+        "Pomodoro",  # app_name
+        0,  # replaces_id, The optional notification ID that this notification replaces
+        "",  # app_icon
+        message,  # summary
+        "",  # body
+        [],  # actions
+        {},  # hints
+        -1,  # expire_timeout, -1 means default
+    )
+    subprocess.run(
+        args="mpv /usr/share/sounds/Oxygen-Sys-App-Error-Critical.ogg --script-opts=autoload-disabled=yes",
+        shell=True,
+        capture_output=True,
+    )
+
+
 def render_progress(text: str, duration: timedelta):
-    """Note this function will block"""
+    """
+    Note: this function will block.
+
+    Performance WARNING: Using rich's progress bar causes about 2% CPU usage.
+    Without it, the CPU usage is nearly 0%.
+    We should see if we could improve this.
+    """
     secs = int(duration.total_seconds())
     now = datetime.now()
     print(f"{now.isoformat(' ', timespec='minutes')}: {text}")
-    for n in track(range(secs), description=text):
+    for _ in track(range(secs), description=text):
         time.sleep(1)
     end = datetime.now()
-    print(f"{end - now} elapsed")
-    print(end="\a")  # beep!
-    sys.stdout.flush()  # Otherwise the beep will be triggered after the sleep
+    print(f"{end - now} elapsed\n")
 
 
 if __name__ == "__main__":
@@ -46,6 +73,10 @@ if __name__ == "__main__":
         arg_0 = inhbit()
         render_progress("Start working..", timedelta(minutes=25))
         uninhibit(arg_0)
+        # print(end="\a") will not work with rich..
+        # So I choose to send a notifiction instead.
+        notify("Time to take a break!")
         render_progress("Start resting..", timedelta(minutes=5))
         input("Press Enter to confirm next cycle...")
+        notify("Time to work again!")
         print()
